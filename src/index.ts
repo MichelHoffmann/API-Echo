16429188146446eabc3764b1ab83698fb0f85331
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-09-03 16:16:56 -0500

    feat: criado servidor express com uma rota inicial.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Olá mundo");
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
