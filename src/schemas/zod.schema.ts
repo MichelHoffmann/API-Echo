50eb072cdcd551ea23b659e88a10b5b5372d8dce
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-09-15 05:45:37 -0500

    feat: add schema for zod validation

---
+++ src/schemas/zod.schema.ts
@@ -0,0 +1,7 @@
+import * as z from "zod";
+
+const userSchema = z.object({
+  name: z.string().min(3, "Name must be at leats 3 characters long"),
+  email: z.email(),
+  password: z.string().min(6),
+});
